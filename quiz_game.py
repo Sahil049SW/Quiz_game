c1cfccb4cab654bd7604ae1cf1d740198fc15188
Sahil049SW <167104757+Sahil049SW@users.noreply.github.com>
2024-04-22 10:50:12 +0530

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,23 @@
+playing = input("Do you wanna Play? ")
+if playing != "yes":
+    quit()
+    
+print("Okay lets play:) ")
+answer = input("Who is the best cricketer in the world? ")
+if answer == "virat kohli":
+    print("Great..!")
+else:
+    print("You are wrong..!")
+
+answer1= input("Who is the best Mid-fielder in the world ")
+if answer1 == "kevin de bruyna" :
+    print("great you are doing great:) ")
+else:
+    print("you got close better luck next time:) ")
+
+answer2= input("who is your fevourite team in IPL ")
+if answer2 == "RCB" :
+    print("vibe match buddy:) ")
+else:
+    print("bug off ") 
+
